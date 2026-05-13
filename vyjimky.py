acc03eb0e2e955ad64bd62a869d5132ad08147cc
davidsherman739-tech <davidsherman739@gmail.com>
2026-05-13 20:08:34 +0200

    Update and rename vyjimky to vyjimky.py

---
+++ vyjimky.py
@@ -0,0 +1,13 @@
+
+    cislo1 = int(input("Zadej první číslo: "))
+    cislo2 = int(input("Zadej druhé číslo: "))
+
+    vysledek = cislo1 / cislo2
+
+    print(f"Výsledek je: {vysledek}")
+
+except ValueError:
+    print("Musíš zadat číslo.")
+
+except ZeroDivisionError:
+    print("Nelze dělit nulou.")
